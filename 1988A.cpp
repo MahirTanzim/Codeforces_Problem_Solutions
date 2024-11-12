70d4c23f3df68a6430a9cea24d34d40664c31fd6
M. B. Mahir Tanzim <tanzimm651@gmail.com>
2024-11-12 19:37:24 +0600

    Create 1988A.cpp

---
+++ 1988A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--){
+		int n, k;
+		cin >> n >> k;
+		int operation = 0;
+		while(n>1){
+			n-=(k-1);
+			operation++;
+		}
+		cout << operation << endl;
+	}
+	return 0;
+}
